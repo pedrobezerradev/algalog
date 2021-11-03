e97e353a4412ed2d97ebd86923b4a27d54ed8904
Pedro Bezerra <eugenissimo@outlook.com>
2021-11-03 17:57:20 -0300

    cpl2.2

--- src/main/java/com/algaworks/algalog/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,11 @@ import com.algaworks.algalog.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Pedro2");
-		cliente1.setTelefone("111111111");
-		cliente1.setEmail("pedro@pedro.com");
-
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Camila");
-		cliente2.setTelefone("222222222");
-		cliente2.setEmail("camila@camila.com");
-
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
